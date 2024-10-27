0e2f08fd6a23531547da9f28558298f03aa439cd
Nikolay.Vinokurov <nikolay.vinokurov@ics-it.ru>
2024-10-27 18:44:27 +0300

    Fix api url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('https://api.nicko-vin.nomorepartiesco.ru/');
+  const api = new Api('https://api.nicko-vin.nomorepartiesco.ru');
 
   export default api;
