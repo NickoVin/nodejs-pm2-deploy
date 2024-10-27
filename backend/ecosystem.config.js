61fcda39939c7fe4e9ee72dce23933a75f4dd36c
Nikolay.Vinokurov <nikolay.vinokurov@ics-it.ru>
2024-10-27 16:30:46 +0300

    Added backend deploy

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,29 @@
+require('dotenv').config({ path: '../.env.deploy' });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF,
+  REPO_URL,
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: 'backend',
+      script: 'dist/app.js',
+    },
+  ],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: REPO_URL,
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+    },
+  },
+};
